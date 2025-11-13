53e6fad26f2f0d64293210cccbdfe0c191834fe5
Safa06 <sukonna.safa@gmail.com>
2025-11-13 21:06:59 +0600

    first commit

---
+++ .history/index_20251111202542.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://health_tracker_db:A8gSEvETOaH84Gua@cluster0.2pjciaj.mongodb.net/?appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("assign-10");
+    const modelCollection = db.collection("all_habits");
+
+      
+    //   Task-1- show latest 6 featured items card
+    // latest 6 data
+    // get
+    // find
+
+    app.get("/latest-models", async (req, res) => {
+      const result = await modelCollection
+        .find()
+        .sort({ created_at: "desc" })
+        .limit(6)
+        .toArray();
+
+      console.log(result);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
---
+++ .history/index_20251113205600.js
@@ -0,0 +1,205 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require('dotenv').config();
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceKey.json");
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+
+
+
+const uri =
+  `mongodb+srv://${process.env.DB_USERNAME}:@cluster0.2pjciaj.mongodb.net/?appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+//for sdk
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  console.log(authorization);
+  if (!authorization) {
+    return res.status(401).send({
+      message: "Unauthorized access. Token not found",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+  
+  try {
+    await admin.auth().verifyIdToken(token);
+    next();
+  } catch (error) {
+    res.status(401).send({
+      message: "Unauthorized access"
+    });
+  }
+};
+
+
+async function run() {
+  try {
+    //await client.connect();
+
+    const db = client.db("assign-10");
+    const modelCollection = db.collection("all_habits");
+
+    //   Task-1- show latest 6 featured items card
+    // latest 6 data
+    // get
+    // find
+
+    app.get("/6_habits", async (req, res) => {
+      const result = await modelCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+
+      console.log(result);
+      res.send(result);
+    });
+
+
+
+
+    // Task-2- take data in public health page
+    // 1. find (all/many data),
+    //  2. findOne(particular 1 ta data)
+    app.get("/all_habits", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      res.send(result);
+    });
+
+
+    app.get("/all_habits/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+
+      const result = await modelCollection.findOne({ _id: objectId });
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+
+
+
+    // Task - 3 : mongoDB te data pathanor way/post method 2 ta-
+    //  1. insertOne - one data sent
+    //  2. insertMany - many data sent
+
+    app.post("/all_habits", async (req, res) => {
+      const data = req.body;
+      // console.log(data)
+      const result = await modelCollection.insertOne(data);
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+
+
+
+    //Task - 4: to update data in mongoDB, ways are 2 -
+    //1. updateOne
+    // 2. updateMany
+    //PUT
+
+    app.put("/all_habits/:id", async (req, res) => {
+      
+      const { id } = req.params;
+      const data = req.body;
+      //  console.log(id)
+      //  console.log(data)
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const update = {
+        $set: data,
+      };
+
+      const result = await modelCollection.updateOne(filter, update);
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    //Task- 5: MyHabit page
+    app.get("/my-habit", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const result = await modelCollection
+        .find({ created_by: email })
+        .toArray();
+      res.send(result);
+    });
+
+    // delete
+    // deleteOne
+    // deleteMany
+
+    app.delete("/all_habits/:id", async (req, res) => {
+      const { id } = req.params;
+     // const objectId = new ObjectId(id)
+      //const filter = {_id: objectId}
+      const result = await modelCollection.deleteOne({ _id: new ObjectId(id) });
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+
+
+    //search
+    app.get("/search", async (req, res) => {
+      const search_text = req.query.search;
+      const result = await modelCollection
+        .find({ name: { $regex: search_text, $options: "i" } })
+        .toArray();
+      res.send(result);
+    });
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
